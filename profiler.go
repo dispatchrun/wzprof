c830d079a06b24e31c940ebfa867611d811c3fd9
Achille Roussel <achille.roussel@gmail.com>
2023-05-04 12:53:32 -0700

    optimize Before/After correlation
    Signed-off-by: Achille Roussel <achille.roussel@gmail.com>

--- profiler.go
+++ profiler.go
@@ -432,6 +432,7 @@ type hook struct {
 	samplers   []Sampler
 	processors []ProfileProcessor
 	values     []int64
+	samples    []sample
 }
 
 func (p *ProfilerListener) createSample(si experimental.StackIterator, values []int64) sample {
@@ -468,18 +469,20 @@ func (h *hook) Before(ctx context.Context, mod api.Module, fnd api.FunctionDefin
 		if fnd.GoFunction() != nil {
 			s.isIO = true
 		}
-		ctx = context.WithValue(ctx, "sample", s)
+		h.samples = append(h.samples, s)
+		//ctx = context.WithValue(ctx, "sample", s)
 	}
 	return ctx
 }
 
 // After implements experimental.FunctionListener.
 func (h *hook) After(ctx context.Context, mod api.Module, fnd api.FunctionDefinition, err error, results []uint64) {
-	v := ctx.Value("sample")
-	if v == nil {
-		return
-	}
-	sample := v.(sample)
+	// v := ctx.Value("sample")
+	// if v == nil {
+	// 	return
+	// }
+	//sample := v.(sample)
+	sample := h.samples[len(h.samples)-1]
 	deltas := make([]int64, len(sample.values))
 	for i, processor := range h.processors {
 		if processor == nil {
@@ -491,4 +494,5 @@ func (h *hook) After(ctx context.Context, mod api.Module, fnd api.FunctionDefini
 	}
 	sample.values = deltas
 	h.profiler.reportSample(sample)
+	h.samples = h.samples[:len(h.samples)-1]
 }
