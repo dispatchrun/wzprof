dispatchrun/wzprof

commit fb53d28c0409e005a7ecc23c1cccdc750aea8ff2

split CPU and memory profiles (#46)
This PR revisits the top-level APIs exposed by wzprof to split the CPU
and memory profile implementations: the main motivation comes from the
fact that those profilers are very different in nature. The CPU profiler
is meant to measure utilization between two point in time, while the
memory profiler tracks "all time" and "current state" (cumulative
counters and snapshots).

The change reuses all the existing internal algorithms for building
profiles, resolving symbols, etc... notably:
- the `cpu_sample` and `cpu_time` profilers are merged into one profile
with two sets of sampled values (as discussed in #24)
- the memory profiles now include `inuse_space` and `inuse_object` to
match exactly the profiles generated by Go pprof
- sampling is decoupled from the profilers and done by an implementation
of wazero's `experimental.FunctionListenerFactory`
- the change relies on https://github.com/stealthrocket/wazero/pull/1 to
combine CPU and memory profilers when needed

Please take a look at let me know if you'd like to see anything changed!

Fixes #24 
Fixes #27 
Fixes #39

---------

Signed-off-by: Achille Roussel <achille.roussel@gmail.com>

cmd/wzprof/main.go
@@ -17,8 +17,8 @@ package main
 import (
 	"context"
 	"crypto/rand"
+	"flag"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 	_ "net/http/pprof"
@@ -26,177 +26,188 @@ import (
 	"os/signal"
 	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/google/pprof/profile"
-	flag "github.com/spf13/pflag"
 	"github.com/stealthrocket/wzprof"
 	"github.com/tetratelabs/wazero"
+	"github.com/tetratelabs/wazero/experimental"
 	"github.com/tetratelabs/wazero/imports/wasi_snapshot_preview1"
 )
 
 func main() {
-	if err := run(); err != nil {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	if err := run(ctx); err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
 	}
 }
 
-const defaultCPUSampling = 0.2
+const defaultSampleRate = 1.0 / 19
 
 type program struct {
-	WasmPath     string
-	File         string
-	HttpAddr     string
-	CPUSampling  float64
-	CPUIncludeIO bool
-	Profilers    string
-	Mounts       []string
+	filePath   string
+	pprofAddr  string
+	cpuProfile string
+	memProfile string
+	sampleRate float64
+	hostTime   bool
+	mounts     []string
 }
 
-func (prog program) Execute(ctx context.Context) error {
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
-
-	wasmName := filepath.Base(prog.WasmPath)
-	wasmCode, err := os.ReadFile(prog.WasmPath)
+func (prog *program) run(ctx context.Context) error {
+	wasmName := filepath.Base(prog.filePath)
+	wasmCode, err := os.ReadFile(prog.filePath)
 	if err != nil {
-		return fmt.Errorf("cannot open WASM file at '%s': %w", prog.WasmPath, err)
+		return fmt.Errorf("loading wasm module: %w", err)
 	}
 
-	pfs := []wzprof.Profiler{}
-	pfnames := strings.Split(prog.Profilers, ",")
-	for _, name := range pfnames {
-		switch name {
-		case "mem":
-			pfs = append(pfs, &wzprof.ProfilerMemory{})
-		case "cpu":
-			pfs = append(pfs, &wzprof.ProfilerCPU{Sampling: float32(prog.CPUSampling)})
-		case "cputime":
-			pfs = append(pfs, &wzprof.ProfilerCPUTime{Sampling: float32(prog.CPUSampling), IncludeIO: prog.CPUIncludeIO})
-		}
+	cpu := wzprof.NewCPUProfiler(time.Now, wzprof.EnableHostTime(prog.hostTime))
+	mem := wzprof.NewMemoryProfiler(time.Now)
+
+	var listeners []experimental.FunctionListenerFactory
+	if prog.cpuProfile != "" || prog.pprofAddr != "" {
+		listeners = append(listeners, wzprof.SampledFunctionListenerFactory(prog.sampleRate, cpu))
+	}
+	if prog.memProfile != "" || prog.pprofAddr != "" {
+		listeners = append(listeners, wzprof.SampledFunctionListenerFactory(prog.sampleRate, mem))
 	}
 
-	pl := wzprof.NewProfileListener(pfs...)
-	ctx = pl.Register(ctx)
+	ctx = context.WithValue(ctx,
+		experimental.FunctionListenerFactoryKey{},
+		experimental.MultiFunctionListenerFactory(listeners...),
+	)
 
-	runtime := wazero.NewRuntimeWithConfig(ctx, wazero.NewRuntimeConfigCompiler())
-	defer runtime.Close(ctx)
+	runtime := wazero.NewRuntimeWithConfig(ctx, wazero.NewRuntimeConfig().
+		WithDebugInfoEnabled(true).
+		WithCustomSections(true))
 
-	wasi_snapshot_preview1.MustInstantiate(ctx, runtime)
+	compiledModule, err := runtime.CompileModule(ctx, wasmCode)
+	if err != nil {
+		return fmt.Errorf("compiling wasm module: %w", err)
+	}
 
-	config := wazero.NewModuleConfig().
-		WithStdout(os.Stdout).
-		WithStderr(os.Stderr).
-		WithStdin(os.Stdin).
-		WithRandSource(rand.Reader).
-		WithSysNanosleep().
-		WithSysNanotime().
-		WithSysWalltime().
-		WithArgs(wasmName).
-		WithFSConfig(createFSConfig(prog.Mounts))
+	symbols, err := wzprof.BuildDwarfSymbolizer(compiledModule)
+	if err != nil {
+		return fmt.Errorf("symbolizing wasm module: %w", err)
+	}
 
-	errC := make(chan error)
-	go func() {
-		defer cancel()
-		compiled, err := runtime.CompileModule(ctx, wasmCode)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		pl.PrepareSymbols(compiled)
-
-		defer func() {
-			if err := compiled.Close(ctx); err != nil {
-				log.Printf("error closing module: %v", err)
-			}
-		}()
-
-		instance, err := runtime.InstantiateModule(ctx, compiled, config)
-		if err != nil {
-			// If any error occurs during the module execution, we don't write the profile.
-			errC <- err
-			return
-		}
-
-		if err := instance.Close(ctx); err != nil {
-			log.Printf("error closing instance: %v", err)
-		}
-	}()
-
-	if prog.HttpAddr != "" {
-		http.DefaultServeMux.Handle("/debug/guest", pl)
+	if prog.pprofAddr != "" {
+		pprof := http.NewServeMux()
+		pprof.Handle("/debug/pprof/profile", cpu.NewHandler(prog.sampleRate, symbols))
+		pprof.Handle("/debug/pprof/heap", mem.NewHandler(prog.sampleRate, symbols))
+		pprof.Handle("/wzprof", http.DefaultServeMux)
 
 		go func() {
-			if err := http.ListenAndServe(prog.HttpAddr, nil); err != nil {
+			if err := http.ListenAndServe(prog.pprofAddr, pprof); err != nil {
 				log.Println(err)
 			}
 		}()
 	}
 
-	var modErr error
-	select {
-	case err := <-errC:
-		modErr = err
-	case <-ctx.Done():
-		cancel()
+	if prog.cpuProfile != "" {
+		cpu.StartProfile()
+		defer func() {
+			writeProfile(prog.cpuProfile, cpu.StopProfile(prog.sampleRate, symbols))
+		}()
 	}
 
-	if prog.File != "" && modErr == nil {
-		if err := writeFile(prog.File, pl.BuildProfile()); err != nil {
-			return err
+	if prog.memProfile != "" {
+		defer func() {
+			writeProfile(prog.memProfile, mem.NewProfile(prog.sampleRate, symbols))
+		}()
+	}
+
+	ctx, cancel := context.WithCancelCause(ctx)
+	go func() {
+		defer cancel(nil)
+		wasi_snapshot_preview1.MustInstantiate(ctx, runtime)
+
+		config := wazero.NewModuleConfig().
+			WithStdout(os.Stdout).
+			WithStderr(os.Stderr).
+			WithStdin(os.Stdin).
+			WithRandSource(rand.Reader).
+			WithSysNanosleep().
+			WithSysNanotime().
+			WithSysWalltime().
+			WithArgs(wasmName).
+			WithFSConfig(createFSConfig(prog.mounts))
+
+		instance, err := runtime.InstantiateModule(ctx, compiledModule, config)
+		if err != nil {
+			cancel(fmt.Errorf("instantiating module: %w", err))
+			return
 		}
-	}
+		if err := instance.Close(ctx); err != nil {
+			cancel(fmt.Errorf("closing module: %w", err))
+			return
+		}
+	}()
 
-	return nil
+	<-ctx.Done()
+	return silenceContextCanceled(context.Cause(ctx))
 }
 
-func run() error {
-	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
-	defer cancel()
-
-	var (
-		file         = flag.String("pprof-file", "", "Filename to write profile to")
-		httpAddr     = flag.String("pprof-addr", "", "HTTP server address")
-		cpuSampling  = flag.Float64("cpu-sampling", defaultCPUSampling, "CPU sampling rate")
-		cpuIncludeIO = flag.Bool("cpu-include-io", false, "Include I/O in CPU time profiling")
-		profilers    = flag.String("profilers", "cputime,cpu,mem", "Comma-separated list of profilers to use")
-		mounts       = flag.StringSlice("mount", []string{}, "Comma-separated list of directories to mount (e.g. /tmp:/tmp:ro)")
-		verbose      = flag.Bool("verbose", false, "Verbose logging")
-	)
-
-	flag.Parse()
-
-	log.Default().SetOutput(io.Discard)
-	if *verbose {
-		log.Default().SetOutput(os.Stderr)
+func silenceContextCanceled(err error) error {
+	if err == context.Canceled {
+		err = nil
 	}
+	return err
+}
+
+var (
+	pprofAddr  string
+	cpuProfile string
+	memProfile string
+	sampleRate float64
+	hostTime   bool
+	mounts     string
+)
+
+func init() {
+	log.Default().SetOutput(os.Stderr)
+	flag.StringVar(&pprofAddr, "pprof-addr", "", "Address where to expose a pprof HTTP endpoint.")
+	flag.StringVar(&cpuProfile, "cpuprofile", "", "Write a CPU profile to the specified file before exiting.")
+	flag.StringVar(&memProfile, "memprofile", "", "Write a memory profile to the specified file before exiting.")
+	flag.Float64Var(&sampleRate, "sample-rate", defaultSampleRate, "Set the profile sampling rate (0-1).")
+	flag.BoolVar(&hostTime, "host", false, "Include time spent in host function calls.")
+	flag.StringVar(&mounts, "mount", "", "Comma-separated list of directories to mount (e.g. /tmp:/tmp:ro).")
+}
+
+func run(ctx context.Context) error {
+	flag.Parse()
 
 	args := flag.Args()
 	if len(args) != 1 {
+		// TODO: pring flag usage
 		return fmt.Errorf("usage: wzprof </path/to/app.wasm>")
 	}
-	wasmPath := args[0]
 
-	return program{
-		WasmPath:     wasmPath,
-		File:         *file,
-		HttpAddr:     *httpAddr,
-		CPUSampling:  *cpuSampling,
-		CPUIncludeIO: *cpuIncludeIO,
-		Profilers:    *profilers,
-		Mounts:       *mounts,
-	}.Execute(ctx)
+	return (&program{
+		filePath:   args[0],
+		pprofAddr:  pprofAddr,
+		cpuProfile: cpuProfile,
+		memProfile: memProfile,
+		sampleRate: sampleRate,
+		hostTime:   hostTime,
+		mounts:     split(mounts),
+	}).run(ctx)
 }
 
-func writeFile(fname string, p *profile.Profile) error {
-	log.Printf("writing profile to %s", fname)
-	f, err := os.Create(fname)
-	if err != nil {
-		return err
+func split(s string) []string {
+	if s == "" {
+		return nil
 	}
-	defer f.Close()
+	return strings.Split(s, ",")
+}
 
-	return p.Write(f)
+func writeProfile(path string, prof *profile.Profile) {
+	if err := wzprof.WriteProfile(path, prof); err != nil {
+		log.Fatalf("ERROR: writing profile: %s", err)
+	}
 }
 
 func createFSConfig(mounts []string) wazero.FSConfig {

.gitignore
@@ -4,6 +4,7 @@
 *.dll
 *.so
 *.dylib
+/wzprof
 
 # Test binary, built with `go test -c`
 *.test

cmd/wzprof/main_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/google/pprof/profile"
@@ -16,9 +17,9 @@ import (
 // that.
 
 func TestDataCSimple(t *testing.T) {
-	expectedSamples := []sample{
+	testMemoryProfiler(t, "../../testdata/c/simple.wasm", []sample{
 		{
-			[]int64{1, 10},
+			[]int64{10, 1},
 			[]frame{
 				{"malloc", 0, false},
 				{"func1", 6, false},
@@ -28,7 +29,7 @@ func TestDataCSimple(t *testing.T) {
 			},
 		},
 		{
-			[]int64{1, 20},
+			[]int64{20, 1},
 			[]frame{
 				{"malloc", 0, false},
 				{"func21", 12, false},
@@ -39,7 +40,7 @@ func TestDataCSimple(t *testing.T) {
 			},
 		},
 		{
-			[]int64{1, 30},
+			[]int64{30, 1},
 			[]frame{
 				{"malloc", 0, false},
 				{"func31", 29, true},
@@ -49,21 +50,13 @@ func TestDataCSimple(t *testing.T) {
 				{"_start", 0, false},
 			},
 		},
-	}
-
-	prog := program{
-		WasmPath:    "../../testdata/c/simple.wasm",
-		CPUSampling: 1.0,
-		Profilers:   "cpu,mem",
-	}
-
-	testFullProfile(t, expectedSamples, prog)
+	})
 }
 
 func TestDataRustSimple(t *testing.T) {
-	expectedSamples := []sample{
+	testMemoryProfiler(t, "../../testdata/rust/simple/target/wasm32-wasi/debug/simple.wasm", []sample{
 		{
-			[]int64{1, 120},
+			[]int64{120, 1},
 			[]frame{
 				{"malloc", 0, false},                                                                        // malloc
 				{"std:sys:wasi:alloc:{impl#0}:alloc", 381, true},                                            // _ZN3std3sys4wasi5alloc81_$LT$impl$u20$core..alloc..global..GlobalAlloc$u20$for$u20$std..alloc..System$GT$5alloc17hf06d843ee28c936eE
@@ -94,41 +87,40 @@ func TestDataRustSimple(t *testing.T) {
 				{"_start", 0, false},                                                                        // _start
 			},
 		},
-	}
-
-	prog := program{
-		WasmPath:    "../../testdata/rust/simple/target/wasm32-wasi/debug/simple.wasm",
-		CPUSampling: 1.0,
-		Profilers:   "cpu,mem",
-	}
-
-	testFullProfile(t, expectedSamples, prog)
+	})
 }
 
-func testFullProfile(t *testing.T, expectedSamples []sample, prog program) {
-	profilePath := t.TempDir() + "/profile.pb.gz"
-	prog.File = profilePath
-	err := prog.Execute(context.Background())
-	if err != nil {
-		t.Fatal("unexpected error:", err)
+func testMemoryProfiler(t *testing.T, path string, expectedSamples []sample) {
+	prog := &program{
+		filePath:   path,
+		sampleRate: 1,
+		memProfile: filepath.Join(t.TempDir(), "mem.pprof"),
 	}
 
-	f, err := os.Open(profilePath)
+	err := prog.run(context.Background())
 	if err != nil {
-		t.Fatalf("can't open profile file %s: %s", profilePath, err)
+		t.Fatal(err)
+	}
+
+	f, err := os.Open(prog.memProfile)
+	if err != nil {
+		t.Fatalf("can't open profile file %s: %s", prog.memProfile, err)
 	}
 	defer f.Close()
 	p, err := profile.Parse(f)
 	if err != nil {
 		t.Fatalf("error parsing profile: %s", err)
 	}
-
-	err = p.CheckValid()
-	if err != nil {
-		t.Errorf("invalid profile: %s", err)
+	if err := p.CheckValid(); err != nil {
+		t.Fatalf("invalid profile: %s", err)
 	}
 
-	expectedTypes := []string{"cpu_samples", "alloc_space"}
+	expectedTypes := []string{
+		"alloc_space",
+		"alloc_objects",
+		"inuse_space",
+		"inuse_objects",
+	}
 
 	if len(p.SampleType) != len(expectedTypes) {
 		t.Errorf("expected %d sample types; got %d", len(expectedTypes), len(p.SampleType))
@@ -140,7 +132,7 @@ func testFullProfile(t *testing.T, expectedSamples []sample, prog program) {
 		}
 	}
 
-	//	printSamples(p.Sample)
+	// printSamples(p.Sample)
 
 	// TODO: pre-process samples to assess faster.
 expected:
@@ -165,6 +157,7 @@ expected:
 				}
 			}
 			if len(stack) == 0 {
+				// TODO: test "inuse_*" samples
 				for i, e := range expected.values {
 					if e != actual.Value[i] {
 						t.Errorf("expected sample matched %d, but value %d was %d; expected %d", si, i, actual.Value[i], e)

cpu.go
@@ -15,52 +15,209 @@
 package wzprof
 
 import (
+	"context"
+	"net/http"
+	"strconv"
+	"sync"
 	"time"
 
 	"github.com/google/pprof/profile"
 	"github.com/tetratelabs/wazero/api"
+	"github.com/tetratelabs/wazero/experimental"
 )
 
-// ProfilerCPU is a sampled based CPU profiler. This profiler is counting
-// the stacks for each functions. The sampling rate is between 0.0 and 1.0.
+// CPUProfiler is the implementation of a performance profiler recording
+// samples of CPU time spent in functions of a WebAssembly module.
 //
-// CPU samples profiles provide a good overview of the CPU usage of a program.
-// while keeping the overhead low.
-type ProfilerCPU struct {
-	// Sampling rate between 0.0 and 1.0
-	Sampling float32
+// The profiler generates samples of two types:
+// - "cpu" records the time spent in function calls (in nanoseconds)
+// - "sample" counts the number of function calls
+type CPUProfiler struct {
+	mutex  sync.Mutex
+	counts stackCounterMap
+	frames []cpuTimeFrame
+	traces []stackTrace
+	time   func() time.Time
+	epoch  time.Time
+	host   bool
 }
 
-type cpuprocessor struct{}
+// CPUProfilerOption is a type used to represent configuration options for
+// CPUProfiler instances created by NewCPUProfiler.
+type CPUProfilerOption func(*CPUProfiler)
 
-func (p cpuprocessor) Before(mod api.Module, params []uint64) int64 {
-	return 1
+// EnableHostTime confiures a CPU time profiler to acount for time spent
+// in calls to host functions.
+//
+// Default to false.
+func EnableHostTime(enable bool) CPUProfilerOption {
+	return func(p *CPUProfiler) { p.host = enable }
 }
 
-func (p cpuprocessor) After(in int64, results []uint64) int64 {
-	return in
+type cpuTimeFrame struct {
+	start int64
+	trace stackTrace
 }
 
-func (p *ProfilerCPU) Register() map[string]ProfileProcessor {
-	return map[string]ProfileProcessor{
-		"count1": cpuprocessor{},
+// NewCPUProfiler constructs a new instance of CPUProfiler using the
+// given time function to record the CPU time consumed.
+//
+// The time function is expected to generate time.Time values embedding a
+// monotonic clock to support computing accurate time deltas. time.Now is a
+// valid function to construct the profiler with.
+func NewCPUProfiler(time func() time.Time, options ...CPUProfilerOption) *CPUProfiler {
+	p := &CPUProfiler{
+		time: time,
 	}
+	for _, opt := range options {
+		opt(p)
+	}
+	return p
 }
 
-func (p *ProfilerCPU) Listen(name string) string {
-	return "count1"
+// StartProfile begins recording the CPU profile. The method returns a boolean
+// to indicate whether starting the profile suceeded (e.g. false is returned if
+// it was already started).
+func (p *CPUProfiler) StartProfile() bool {
+	p.mutex.Lock()
+	defer p.mutex.Unlock()
+
+	if p.counts != nil {
+		return false // already started
+	}
+
+	p.counts = make(stackCounterMap)
+	p.epoch = p.time()
+	return true
 }
 
-func (p *ProfilerCPU) SampleType() *profile.ValueType {
-	return &profile.ValueType{Type: "cpu_samples", Unit: "count"}
+// StopProfile stops recording and returns the CPU profile. The method returns
+// nil if recording of the CPU profile wasn't started.
+func (p *CPUProfiler) StopProfile(sampleRate float64, symbols Symbolizer) *profile.Profile {
+	p.mutex.Lock()
+	samples, epoch := p.counts, p.epoch
+	p.counts = nil
+	p.mutex.Unlock()
+
+	if samples == nil {
+		return nil
+	}
+
+	if !p.host {
+		for k, sample := range samples {
+			if sample.stack.host() {
+				delete(samples, k)
+				for _, other := range samples {
+					if sample.stack.contains(other.stack) {
+						other.subtract(sample.total())
+					}
+				}
+			}
+		}
+	}
+
+	return buildProfile(sampleRate, symbols, samples, epoch, p.time(),
+		[]*profile.ValueType{
+			{Type: "cpu", Unit: "nanoseconds"},
+			{Type: "samples", Unit: "count"},
+		},
+	)
 }
 
-func (p *ProfilerCPU) Sampler() Sampler {
-	return newRandomSampler(time.Now().UnixNano(), p.Sampling)
+// NewHandler returns a http handler allowing the profiler to be exposed on a
+// pprof-compatible http endpoint.
+//
+// The sample rate is a value between 0 and 1 used to scale the profile results
+// based on the sampling rate applied to the profiler so the resulting values
+// remain representative.
+//
+// The symbolizer passed as argument is used to resolve names of program
+// locations recorded in the profile.
+func (p *CPUProfiler) NewHandler(sampleRate float64, symbols Symbolizer) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		duration := 30 * time.Second
+
+		if seconds := r.FormValue("seconds"); seconds != "" {
+			n, err := strconv.ParseInt(seconds, 10, 64)
+			if err == nil && n > 0 {
+				duration = time.Duration(n) * time.Second
+			}
+		}
+
+		ctx := r.Context()
+		deadline, ok := ctx.Deadline()
+		if ok {
+			if timeout := time.Until(deadline); duration > timeout {
+				serveError(w, http.StatusBadRequest, "profile duration exceeds server's WriteTimeout")
+				return
+			}
+		}
+
+		if !p.StartProfile() {
+			serveError(w, http.StatusInternalServerError, "Could not enable CPU profiling: profiler already running")
+			return
+		}
+
+		timer := time.NewTimer(duration)
+		select {
+		case <-timer.C:
+		case <-ctx.Done():
+		}
+		timer.Stop()
+		serveProfile(w, p.StopProfile(sampleRate, symbols))
+	})
 }
 
-func (p *ProfilerCPU) PostProcess(prof *profile.Profile, idx int, offLocations []*profile.Location) {
-	// no post process for this profiler
+// NewListener returns a function listener suited to record CPU timings of
+// calls to the function passed as argument.
+func (p *CPUProfiler) NewListener(def api.FunctionDefinition) experimental.FunctionListener {
+	return cpuListener{p}
 }
 
-var _ Profiler = &ProfilerCPU{}
+type cpuListener struct{ *CPUProfiler }
+
+func (p cpuListener) now() int64 {
+	return int64(p.time().Sub(p.epoch))
+}
+
+func (p cpuListener) Before(ctx context.Context, mod api.Module, def api.FunctionDefinition, params []uint64, si experimental.StackIterator) context.Context {
+	var frame cpuTimeFrame
+	p.mutex.Lock()
+
+	if p.counts != nil {
+		start := p.now()
+		trace := stackTrace{}
+
+		if i := len(p.traces); i > 0 {
+			i--
+			trace = p.traces[i]
+			p.traces = p.traces[:i]
+		}
+
+		frame = cpuTimeFrame{
+			start: start,
+			trace: makeStackTrace(trace, si),
+		}
+	}
+
+	p.frames = append(p.frames, frame)
+	p.mutex.Unlock()
+	return ctx
+}
+
+func (p cpuListener) After(ctx context.Context, mod api.Module, def api.FunctionDefinition, err error, results []uint64) {
+	p.mutex.Lock()
+
+	i := len(p.frames) - 1
+	f := p.frames[i]
+	p.frames = p.frames[:i]
+
+	if f.start != 0 {
+		if p.counts != nil {
+			p.counts.observe(f.trace, p.now()-f.start)
+		}
+		p.traces = append(p.traces, f.trace)
+	}
+
+	p.mutex.Unlock()
+}

cputime.go
@@ -1,99 +1,0 @@
-//  Copyright 2023 Stealth Rocket, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-package wzprof
-
-import (
-	"time"
-
-	"github.com/google/pprof/profile"
-	"github.com/tetratelabs/wazero/api"
-)
-
-// ProfileProcessor records the active CPU time of the WASM guest.
-// This profiler doesn't take into account the time "off-cpu" (e.g. waiting for I/O).
-// Here, all host-functions are considered "off-cpu" from a guest perspective.
-type ProfilerCPUTime struct {
-	// Sampling rate between 0.0 and 1.0.
-	Sampling float32
-
-	// If true, the profiler will account for time spent in host-functions.
-	IncludeIO bool
-}
-
-// https://pkg.go.dev/time#hdr-Monotonic_Clocks
-var epoch = time.Now()
-
-type cputime struct{}
-
-func (p cputime) Before(mod api.Module, params []uint64) int64 {
-	return int64(time.Since(epoch))
-}
-
-func (p cputime) After(in int64, results []uint64) int64 {
-	return int64(time.Since(epoch)) - in
-}
-
-func (p *ProfilerCPUTime) Register() map[string]ProfileProcessor {
-	return map[string]ProfileProcessor{
-		"time": cputime{},
-	}
-}
-
-func (p *ProfilerCPUTime) Listen(name string) string {
-	return "time"
-}
-
-func (p *ProfilerCPUTime) SampleType() *profile.ValueType {
-	return &profile.ValueType{Type: "cpu_time", Unit: "nanosecond"}
-}
-
-func (p *ProfilerCPUTime) Sampler() Sampler {
-	return newRandomSampler(time.Now().UnixNano(), p.Sampling)
-}
-
-// PostProcess removes all samples that are "off-cpu" aka all time spent executing host-functions.
-func (p *ProfilerCPUTime) PostProcess(prof *profile.Profile, idx int, offLocations []*profile.Location) {
-	if p.IncludeIO {
-		return
-	}
-
-	for _, off := range offLocations {
-		p.processSamples(prof, off)
-	}
-}
-
-func (p *ProfilerCPUTime) processSamples(prof *profile.Profile, off *profile.Location) {
-	var match bool
-	for _, s := range prof.Sample {
-		for _, loc := range s.Location {
-			match = false
-			if loc == off {
-				match = true
-				break
-			}
-		}
-		if match {
-			for _, loc := range s.Location {
-				for _, sample := range prof.Sample {
-					if loc == sample.Location[0] {
-						sample.Value[0] = 0
-					}
-				}
-			}
-		}
-	}
-}
-
-var _ Profiler = &ProfilerCPUTime{}

dwarf.go
@@ -23,8 +23,15 @@ import (
 	"math"
 	"sort"
 
+	"github.com/tetratelabs/wazero"
 	"github.com/tetratelabs/wazero/api"
 )
+
+// BuildDwarfSymbolizer constructs a Symbolizer instance from the DWARF sections
+// of the given WebAssembly module.
+func BuildDwarfSymbolizer(module wazero.CompiledModule) (Symbolizer, error) {
+	return newDwarfmapper(module.CustomSections())
+}
 
 type pcrange = [2]uint64
 
@@ -50,7 +57,7 @@ type dwarfmapper struct {
 	subprograms []subprogramRange
 }
 
-func newDwarfmapper(sections []api.CustomSection) (mapper, error) {
+func newDwarfmapper(sections []api.CustomSection) (*dwarfmapper, error) {
 	var info, line, ranges, str, abbrev []byte
 
 	for _, section := range sections {
@@ -218,11 +225,7 @@ func (d *dwarfparser) parseSubprogram(cu *dwarf.Entry, ns string, e *dwarf.Entry
 	}
 }
 
-// Lookup returns a list of function locations for a given program
-// counter, starting from current function followed by the inlined
-// functions, in order of inlining. Result if empty if the pc cannot
-// be resolved in the dwarf data.
-func (d *dwarfmapper) Lookup(pc uint64) []location {
+func (d *dwarfmapper) LocationsForPC(pc uint64) []Location {
 	// TODO: replace with binary search
 
 	var spgm *subprogram
@@ -293,8 +296,8 @@ func (d *dwarfmapper) Lookup(pc uint64) []location {
 	}
 
 	human, stable := d.namesForSubprogram(spgm.Entry, spgm)
-	locations := make([]location, 0, 1+len(spgm.Inlines))
-	locations = append(locations, location{
+	locations := make([]Location, 0, 1+len(spgm.Inlines))
+	locations = append(locations, Location{
 		File:       le.File.Name,
 		Line:       int64(le.Line),
 		Column:     int64(le.Column),
@@ -317,7 +320,7 @@ func (d *dwarfmapper) Lookup(pc uint64) []location {
 			line, _ := f.Val(dwarf.AttrCallLine).(int64)
 			col, _ := f.Val(dwarf.AttrCallLine).(int64)
 			human, stable := d.namesForSubprogram(f, nil)
-			locations = append(locations, location{
+			locations = append(locations, Location{
 				File:       file.Name,
 				Line:       line,
 				Column:     col,

go.mod
@@ -5,8 +5,7 @@ go 1.20
 require (
 	github.com/google/pprof v0.0.0-20230406165453-00490a63f317
 	github.com/tetratelabs/wazero v1.0.3
+	golang.org/x/exp v0.0.0-20230425010034-47ecfdc1ba53
 )
 
-require github.com/spf13/pflag v1.0.5
-
-replace github.com/tetratelabs/wazero => github.com/stealthrocket/wazero v0.0.0-20230503192533-6860dd7061a1
+replace github.com/tetratelabs/wazero => github.com/stealthrocket/wazero v0.0.0-20230506195512-778fba8a2815

go.sum
@@ -1,6 +1,6 @@
 github.com/google/pprof v0.0.0-20230406165453-00490a63f317 h1:hFhpt7CTmR3DX+b4R19ydQFtofxT0Sv3QsKNMVQYTMQ=
 github.com/google/pprof v0.0.0-20230406165453-00490a63f317/go.mod h1:79YE0hCXdHag9sBkw2o+N/YnZtTkXi0UT9Nnixa5eYk=
-github.com/spf13/pflag v1.0.5 h1:iy+VFUOCP1a+8yFto/drg2CJ5u0yRoB7fZw3DKv/JXA=
-github.com/spf13/pflag v1.0.5/go.mod h1:McXfInJRrz4CZXVZOBLb0bTZqETkiAhM9Iw0y3An2Bg=
-github.com/stealthrocket/wazero v0.0.0-20230503192533-6860dd7061a1 h1:uVp9P2LrlW2pggzehxJApl+7bF0v96z2GnaWMfc8gM8=
-github.com/stealthrocket/wazero v0.0.0-20230503192533-6860dd7061a1/go.mod h1:wYx2gNRg8/WihJfSDxA1TIL8H+GkfLYm+bIfbblu9VQ=
+github.com/stealthrocket/wazero v0.0.0-20230506195512-778fba8a2815 h1:iUDXJFQpOtGA+3/OOWaOAi2OhkLmIQe3TmnmOYcJU0w=
+github.com/stealthrocket/wazero v0.0.0-20230506195512-778fba8a2815/go.mod h1:wYx2gNRg8/WihJfSDxA1TIL8H+GkfLYm+bIfbblu9VQ=
+golang.org/x/exp v0.0.0-20230425010034-47ecfdc1ba53 h1:5llv2sWeaMSnA3w2kS57ouQQ4pudlXrR0dCgw51QK9o=
+golang.org/x/exp v0.0.0-20230425010034-47ecfdc1ba53/go.mod h1:V1LtkGg67GoY2N1AnLN78QLrzxkLyJw7RJb1gzOOz9w=

commit 0ebac7f8a2027f4487abe6e0a32755ec5bae5a01

Allow wzprof to run without dwarf
Regression from #46. By turning this error into a warning, we allow wzprof to be
used on modules that don't have DWARF, such as those produced by golang/go.

cmd/wzprof/main.go
@@ -85,7 +85,8 @@ func (prog *program) run(ctx context.Context) error {
 
 	symbols, err := wzprof.BuildDwarfSymbolizer(compiledModule)
 	if err != nil {
-		return fmt.Errorf("symbolizing wasm module: %w", err)
+		symbols = nil
+		log.Println("warning: symbolizing wasm module:", err)
 	}
 
 	if prog.pprofAddr != "" {